0ac9aa1005b3863ba7485de12965628c8cf33727
Rizki Samsul <rizkisamsul@gmail.com>
2023-10-13 05:45:51 +0700

    update

--- interfaces/api.py
+++ interfaces/api.py
@@ -10,22 +10,18 @@ def home():
 
 @app.route('/insert')
 def insert():
-    data = request.args.get('data')
-    print(data)
+    username = request.args.get('username')
+    print(username)
     fin = open("cmd1.sh", "rt")
     fout = open("cmd1_out.sh", "w+")
     for line in fin:
-        JsonDump = json.dumps('{"Args":["Store", "1234", "{asdad:1234}"]}')
-        # JsonDump = json.dumps('{"Args":["Query", "default-asset"]}')
-        line = line.replace('PARAMETER', JsonDump)
-        line = line.replace('"{','{')
-        line = line.replace('}"','}')
+        line = line.replace('VAR_NIK','1234')
         print(line)
         fout.write(line)
     fin.close()
     fout.close()
     subprocess.run ( [ "sh", "cmd1_out.sh"] )
-    return data
+    return username
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",  port=8000, debug=True)

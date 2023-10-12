2cc78d96ee62e880d495ec0fa265961b324ee83d
Rizki Samsul <rizkisamsul@gmail.com>
2023-10-13 06:44:07 +0700

    update

--- chaincode-as-external-service/asset.go
+++ chaincode-as-external-service/asset.go
@@ -1,10 +1,11 @@
 package main
 
-// SimpleAsset ...
 type SimpleAsset struct {
-	TxID    string `json:"txID"`
-	FullName string `json:"fullname"`
-	DateTime string `json:"datetime"`
-	Transaction string `json:"transaction"`
-	Value string `json:"value"`
+	TxID    			string `json:"txID"`
+	UID 				string `json:"uid"`
+	UID_DESTINATION		string `json:"uid_destination"`
+	TransactionID 		string `json:"transaction_id"`
+	TransactionType 	string `json:"transaction_type"`
+	TransactionAmount 	string `json:"transaction_amount"`
+	TransactionDateTime string `json:"transaction_date_time"`
 }
--- interfaces/api.py
+++ interfaces/api.py
@@ -1,27 +1,47 @@
 import subprocess
 import json
+import hashlib
+import random
+import string
+import datetime
+import uuid
+
+from datetime import datetime
 from flask import request
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'block-chain apis'
 
-@app.route('/insert')
+@app.route('/insert', methods = ['POST'])
 def insert():
-    username = request.args.get('username')
-    print(username)
-    fin = open("cmd1.sh", "rt")
-    fout = open("cmd1_out.sh", "w+")
+    uid = request.args.get('uid')
+    uid_destination = request.args.get('uid_destination')
+    transaction_id = request.args.get('transaction_id')
+    transaction_type = request.args.get('transaction_type')
+    transaction_amount = request.args.get('transaction_amount')
+
+    now = datetime.now()
+    transaction_date_time = now.strftime("%d/%m/%Y %H:%M:%S")
+    filename = "logs/"+str(uuid.uuid4())+".sh"
+
+    fin = open("InsertQuery.sh", "rt")
+    fout = open(filename, "w+")
     for line in fin:
-        line = line.replace('VAR_NIK','1234')
-        print(line)
+        line = line.replace('VAR_UID', uid)
+        line = line.replace('VAR_DESTINATION', uid_destination)
+        line = line.replace('VAR_TRANSACTION_ID', transaction_id)
+        line = line.replace('VAR_TRANSACTION_TYPE', transaction_type)
+        line = line.replace('VAR_TRANSACTION_AMOUNT', transaction_amount)
+        line = line.replace('VAR_TRANSACTION_DATETIME', transaction_date_time)
         fout.write(line)
     fin.close()
     fout.close()
-    subprocess.run ( [ "sh", "cmd1_out.sh"] )
-    return username
+    subprocess.run ( [ "sh", filename] )
+    return transaction_id
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",  port=8000, debug=True)

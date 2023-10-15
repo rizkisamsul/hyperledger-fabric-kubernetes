81ffec08de9c60daabe07e15b0bacd0224bb4486
Rizki Samsul <rizkisamsul@gmail.com>
2023-10-15 10:11:45 +0700

    updat

--- chaincode-as-external-service/asset.go
+++ chaincode-as-external-service/asset.go
@@ -3,7 +3,8 @@ package main
 type SimpleAsset struct {
 	TxID    			string `json:"txID"`
 	UID 				string `json:"uid"`
-	UID_DESTINATION		string `json:"uid_destination"`
+	DESTINATION_UID		string `json:"destination_uid"`
+	DESTINATION_TYPE	string `json:"destination_type"`
 	TransactionID 		string `json:"transaction_id"`
 	TransactionType 	string `json:"transaction_type"`
 	TransactionAmount 	string `json:"transaction_amount"`
--- interfaces/api.py
+++ interfaces/api.py
@@ -9,9 +9,19 @@ import uuid
 from datetime import datetime
 from flask import request
 from flask import Flask, render_template
+from flask import jsonify, make_response
 
 import psycopg2
 from datetime import datetime
+token_security = "cbdcitb"
+
+cbdcID = "e702e3530f81e19b73ce331173638c00"
+bankBalance = "a50493399674fb1c80eaa6f5c0bfc339"
+
+insert_query = """
+    INSERT INTO transaction.logs (uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time)
+    VALUES (%s, %s, %s, %s, %s, %s) RETURNING id
+"""
 
 try:
     conn = psycopg2.connect(
@@ -25,51 +35,200 @@ except Exception as e:
     print(f"Error: Unable to connect to the database: {e}")
     exit()
 
-cur = conn.cursor()
+cursor = conn.cursor()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'block-chain apis'
 
-@app.route('/insert', methods = ['POST'])
-def insert():
-    uid = request.args.get('uid')
-    uid_destination = request.args.get('uid_destination')
-    transaction_id = request.args.get('transaction_id')
-    transaction_type = request.args.get('transaction_type')
-    transaction_amount = request.args.get('transaction_amount')
+def TrxCBDC(uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time):
+    try:
+        cursor.execute(
+            insert_query,
+            (uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
 
-    # uid = 'user1'
-    # uid_destination = 'user2'
-    # transaction_id = 'trans1'
-    # transaction_type = 'credit'
-    # transaction_amount = 100.50
-    # transaction_date_time = datetime.now()
+def convertCBDC(uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time):
+    try:
+        cursor.execute(
+            insert_query,
+            (uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
 
-    # insert_query = """
-    #     INSERT INTO transactions (
-    #         uid, uid_destination, transaction_id,
-    #         transaction_type, transaction_amount, transaction_date_time
-    #     )
-    #     VALUES (%s, %s, %s, %s, %s, %s);
-    # """
+def convertCBDCApprove(uid, destination_uid, transaction_amount):
+    query = f"SELECT * FROM transaction.logs WHERE transaction_type = 'balance' AND uid = '{cbdcID}' order by id desc LIMIT 1"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    if rows:
+        balance_cbdc = rows[0][4]
+    else:
+        balance_cbdc = 0
 
-    # print(insert_query)
+    query = f"SELECT * FROM transaction.logs WHERE transaction_type = 'convert' AND uid = '{bankBalance}' order by id desc LIMIT 1"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    if rows:
+        balance_convert = rows[0][4]
+    else:
+        balance_convert = 0
 
-    # try:
-    #     cur.execute(
-    #         insert_query,
-    #         (uid, uid_destination, transaction_id, transaction_type, transaction_amount, transaction_date_time)
-    #     )
-    #     inserted_id = cur.fetchone()[0]
-    #     conn.commit()
-    #     print("Data has been inserted successfully")
-    # except Exception as e:
-    #     print(f"Error: Unable to insert data: {e}")
-    #     conn.rollback()
+    balance_cbdc = int(balance_cbdc)
+    
+    if int(balance_convert) >= int(transaction_amount):
+        balance_approve = int(transaction_amount)
+        balance_bank = int(balance_convert) - int(transaction_amount)
+        balance_cbdc = int(balance_cbdc) + int(balance_approve)
+
+    store_uid = uid
+    store_destination_uid = destination_uid
+    store_destination_type = "central"
+    store_transaction_type = "approve-convert"
+    store_transaction_amount = transaction_amount
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
+
+    store_uid = bankBalance
+    store_destination_uid = bankBalance
+    store_destination_type = "system"
+    store_transaction_type = "balance"
+    store_transaction_amount = balance_bank
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
+
+    store_uid = cbdcID
+    store_destination_uid = cbdcID
+    store_destination_type = "system"
+    store_transaction_type = "balance"
+    store_transaction_amount = balance_cbdc
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
 
 
+
+def approveIssue(uid, destination_uid, transaction_amount):
+    query = f"SELECT * FROM transaction.logs WHERE transaction_type = 'balance' AND uid = '{cbdcID}' order by id desc LIMIT 1"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    if rows:
+        balance_cbdc = rows[0][4]
+    else:
+        balance_cbdc = 0
+    
+    query = f"SELECT * FROM transaction.logs WHERE transaction_type = 'balance' AND uid = '{destination_uid}' order by id desc LIMIT 1"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    if rows:
+        balance_user = rows[0][4]
+    else:
+        balance_user = 0
+
+    balance_cbdc = int(balance_cbdc) - int(transaction_amount)
+    balance_user = int(balance_user) + int(transaction_amount)
+
+    store_uid = uid
+    store_destination_uid = destination_uid
+    store_destination_type = "central"
+    store_transaction_type = "approve"
+    store_transaction_amount = transaction_amount
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
+
+    store_uid = destination_uid
+    store_destination_uid = destination_uid
+    store_destination_type = "system"
+    store_transaction_type = "balance"
+    store_transaction_amount = balance_user
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
+
+    store_uid = cbdcID
+    store_destination_uid = cbdcID
+    store_destination_type = "system"
+    store_transaction_type = "balance"
+    store_transaction_amount = balance_cbdc
+    store_transaction_date_time = datetime.now()
+
+    try:
+        cursor.execute(
+            insert_query,
+            (store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+        )
+        transaction_id = cursor.fetchone()[0]
+        conn.commit()
+        print("Data has been inserted successfully")
+    except Exception as e:
+        print(f"Error: Unable to insert data: {e}")
+        conn.rollback()
+
+def blockchainStore(store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time):
     now = datetime.now()
     transaction_date_time = now.strftime("%d/%m/%Y %H:%M:%S")
     filename = "logs/"+str(uuid.uuid4())+".sh"
@@ -78,7 +237,7 @@ def insert():
     fout = open(filename, "w+")
     for line in fin:
         line = line.replace('VAR_UID', uid)
-        line = line.replace('VAR_DESTINATION', uid_destination)
+        line = line.replace('VAR_DESTINATION', destination_uid)
         line = line.replace('VAR_TRANSACTION_ID', transaction_id)
         line = line.replace('VAR_TRANSACTION_TYPE', transaction_type)
         line = line.replace('VAR_TRANSACTION_AMOUNT', transaction_amount)
@@ -88,7 +247,35 @@ def insert():
     fout.close()
     subprocess.run ( [ "sh", filename] )
     
-    return "ok"
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    txid = ""
+    transaction_id = ""
+
+    token = request.args.get('token')
+    if(token != token_security):
+        return jsonify(status=0, message="invalid token")
+
+    uid = request.args.get('uid')
+    destination_uid = request.args.get('destination_uid')
+    destination_type = request.args.get('destination_type')
+    transaction_type = request.args.get('transaction_type')
+    transaction_amount = request.args.get('transaction_amount')
+    transaction_date_time = datetime.now()
+
+    if transaction_type == "approve":
+        approveIssue(uid, destination_uid, transaction_amount)
+    elif transaction_type == "approve-convert":
+        convertCBDCApprove(uid, destination_uid, transaction_amount)
+    elif transaction_type == "convert":
+        convertCBDC(uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time)
+    else:
+        TrxCBDC(uid, destination_uid, destination_type, transaction_type, transaction_amount, transaction_date_time)
+
+    # blockchainStore(store_uid, store_destination_uid, store_destination_type, store_transaction_type, store_transaction_amount, store_transaction_date_time)
+    
+    return jsonify(status=1, message="successfully", transaction_id=transaction_id)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",  port=8000, debug=True)

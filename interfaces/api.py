65bbcd4bcb11ac6d4b74375386b51f292cc58d71
Rizki Samsul <rizkisamsul@gmail.com>
2023-10-16 11:34:19 +0800

    update

--- interfaces/api.py
+++ interfaces/api.py
@@ -411,4 +411,4 @@ def insert():
     return jsonify(status=1, message="successfully", transaction_id=transaction_id)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",  port=8000, debug=True)
+    app.run(host="0.0.0.0",  port=5000, debug=True)
